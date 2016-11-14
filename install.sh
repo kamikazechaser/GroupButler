0313aa93cd809780f41641e57beb3bb6b3834572
Mohammed Sohail <sohailsameja@gmail.com>
2016-11-14 14:16:09 +0530

    test

--- install.sh
+++ install.sh
@@ -23,7 +23,7 @@ else
     
     echo -e "\e[1;36mFetching latest Group Butler source code\e[0m"
     git clone https://github.com/RememberTheAir/GroupButler.git && cd GroupButler
-    sudo chmod 777 launch.sh
+    bash launch.sh
     
     echo -e "\e[1;32mGroup Butler successfully installed!, change values in config file and run ./launch.sh\e[0m"
     echo " "
